8011fffb579a5baf6d78deddb5069f26de1154c9
Abdulla <aansari@codal.com>
2022-12-13 10:45:13 +0530

    Update code

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -77,7 +77,12 @@ export default function Rightbar({ user }) {
         <h4 className="rightbarTitle">Friends to Follow</h4>
         <ul className="rightbarFriendList">
           {friendsToFollow.map((u) => (
-            u.first_name !== "" ? <Link style={{ textDecoration: "none", color: "black" }} key={u.id} to={`/profile/${u?.id}`}><Online user={u} /></Link> : ""
+            u.first_name !== "" ? <><Link style={{ textDecoration: "none", color: "black" }} key={u.id} to={`/profile/${u?.id}`}><Online user={u} /></Link>{user.user.id != currentUser.id && (
+              <button className="rightbarFollowbutton" onClick={handleClick}>
+                {followed ? "UnFollow" : "Follow"}
+                {followed ? <Remove /> : <Add />}
+              </button>
+            )}</> : ""
           ))}
         </ul>
       </>
@@ -122,12 +127,6 @@ export default function Rightbar({ user }) {
                 <span className="rightbarFollowingName">{friend?.first_name} {friend?.last_name}</span>
               </div>
             </Link>
-            {user.user.id != currentUser.id && (
-              <button className="rightbarFollowbutton" onClick={handleClick}>
-                {followed ? "UnFollow" : "Follow"}
-                {followed ? <Remove /> : <Add />}
-              </button>
-            )}
             </>
           ))}
         </div>
